83291a6177a9765a4d8e59461c4374ffb17451ec
JannatN <jannatfaisalnatsheh@gmail.com>
2020-12-07 00:00:23 +0200

    employee design

--- employee.php
+++ employee.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employees</title>
-    <link rel="stylesheet" href="employee.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="employee.css">
 
 
 
@@ -42,49 +44,81 @@
 
 <body>
     <header>Welcome to Employees Section </header><br>
-    <button class="btn" id="btn1" type="submit"> Employees General Info </button>
-    <div class="content"></div>
+    <div id="external">
+        <div class="left">
+            <button class="btn" id="btn1" type="submit"> Employees General Info </button>
+            <div class="content"></div>
+        </div>
+        <div class="middle">
+            <button class="btn">New Employee +</button>
+        </div>
+        <div class="right">
+            <h2>Search By <img src="search.png" alt="serach">
+            </h2>
+            <div class="btn-group">
+                <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Large button
+                </button>
+                <div class="dropdown-menu">
+                    ...
+                </div>
+            </div>
+            <div class="btn-group">
+                <button class="btn btn-secondary btn-lg" type="button">
+                    Large split button
+                </button>
+                <button type="button" class="btn btn-lg btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="sr-only">Toggle Dropdown</span>
+                </button>
+                <div class="dropdown-menu">
+                    ...
+                </div>
+            </div>
 
-    <h2>Search for Employee:</h2>
-    <p>Search by Name <button class="btn" type="submit" name="qe2">Search</button></p>
-    <p>Search by Department-Name <button class="btn">Search</button></p>
-    <p>Search by Title <button class="btn">Search</button></p>
-    <p>Search by Salary <button class="btn">Search</button></p>
-    <button class="btn">New Employee +</button>
+            <p> Name <button type="submit" name="qe2">Search</button></p>
+            <p>Department-Name <button>Search</button></p>
+            <p>Title <button>Search</button></p>
+            <p>Salary <button>Search</button></p>
+            <form class="form-inline active-pink-3 active-pink-4">
+                <i class="fas fa-search" aria-hidden="true"></i>
+                <input class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search">
+            </form>
+        </div>
 
-    <script>
-        $(document).ready(function() {
-            $("#btn1").on('click', function() {
-                $('.content').toggleClass('active');
-                if ($(".content").is(":visible")) {
 
-                    var ajax = new XMLHttpRequest();
-                    ajax.open("GET", "db.php", true);
-                    ajax.send();
+        <script>
+            $(document).ready(function() {
+                $("#btn1").on('click', function() {
+                    $('.content').toggleClass('active');
+                    if ($(".content").is(":visible")) {
 
-                    ajax.onreadystatechange = function() {
-                        if (this.readyState == 4 && this.status == 200) {
-                            var data = JSON.parse(this.responseText);
-                            console.log(data);
+                        var ajax = new XMLHttpRequest();
+                        ajax.open("GET", "db.php", true);
+                        ajax.send();
 
-                            var tableContentHtml = '<table><tr><th>Department Name</th><th>First Name</th> <th>Last Name</th> </tr>';
+                        ajax.onreadystatechange = function() {
+                            if (this.readyState == 4 && this.status == 200) {
+                                var data = JSON.parse(this.responseText);
+                                console.log(data);
 
-                            for (var a = 0; a < data.length; a++) {
-                                var dept_name = data[a].dept_name;
-                                var first_name = data[a].first_name;
-                                var last_name = data[a].last_name;
-                                tableContentHtml += '<tr><td>' + dept_name + '</td>' + '<td>' + first_name + '</td>' + '<td>' + last_name + '</td></tr>';
+                                var tableContentHtml = '<table><tr><th>Department Name</th><th>First Name</th> <th>Last Name</th> </tr>';
+
+                                for (var a = 0; a < data.length; a++) {
+                                    var dept_name = data[a].dept_name;
+                                    var first_name = data[a].first_name;
+                                    var last_name = data[a].last_name;
+                                    tableContentHtml += '<tr><td>' + dept_name + '</td>' + '<td>' + first_name + '</td>' + '<td>' + last_name + '</td></tr>';
+                                }
+                                tableContentHtml += '</table>';
+                                $(tableContentHtml).appendTo(".content");
                             }
-                            tableContentHtml += '</table>';
-                            $(tableContentHtml).appendTo(".content");
+
                         }
+                    };
 
-                    }
-                };
-
+                });
             });
-        });
-    </script>
+        </script>
 </body>
 
 </html>
