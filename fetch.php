5d1b9367275905b0a1f083ebba0bfcdb8ee00e90
JannatN <jannatfaisalnatsheh@gmail.com>
2020-12-13 00:11:52 +0200

    search dept

--- fetch.php
+++ fetch.php
@@ -2,45 +2,43 @@
 //fetch.php
 $connect = mysqli_connect("localhost", "root", "", "employees");
 $output = '';
-if(isset($_POST["query"]))
-{
- $search = mysqli_real_escape_string($connect, $_POST["query"]);
- $query = "
-  SELECT first_name, last_name FROM employees
-  WHERE first_name LIKE '%".$search."%'
-  OR last_name LIKE '%".$search."%' 
- ";
-}
-else
-{
- $query = "
-  SELECT first_name, last_name FROM employees ORDER BY first_name
- ";
+if (isset($_POST["query"])) {
+   $search = mysqli_real_escape_string($connect, $_POST["query"]);
+   //    $query = " SELECT first_name, last_name FROM employees
+   //   WHERE dept_name LIKE '%" . $search . "%'
+   //   OR last_name LIKE '%" . $search . "%'";
+
+   $query = "SELECT first_name, last_name,departments.dept_name 
+FROM employees, departments, dept_emp
+Where dept_emp.emp_no =employees.emp_no 
+AND departments.dept_no =dept_emp.dept_no
+AND departments.dept_name LIKE '$search'";
+} else {
+   $query = "SELECT first_name, last_name, departments.dept_name 
+   FROM employees, departments, dept_emp
+   Where dept_emp.emp_no =employees.emp_no 
+   AND departments.dept_no =dept_emp.dept_no";
 }
 $result = mysqli_query($connect, $query);
-if(mysqli_num_rows($result) > 0)
-{
- $output .= '
+if (mysqli_num_rows($result) > 0) {
+   $output .= '
   <div class="table-responsive">
    <table class="table table bordered">
     <tr>
-     <th>Customer Name</th>
-     <th>lastNAme</th>
+     <th>First Name</th>
+     <th>last NAme</th>
     
     </tr>
  ';
- while($row = mysqli_fetch_array($result))
- {
-  $output .= '
+   while ($row = mysqli_fetch_array($result)) {
+      $output .= '
    <tr>
-    <td>'.$row["first_name"].'</td>
-    <td>'.$row["last_name"].'</td>
+    <td>' . $row["first_name"] . '</td>
+    <td>' . $row["last_name"] . '</td>
    </tr>
   ';
- }
- echo $output;
-}
-else
-{
- echo 'Data Not Found';
+   }
+   echo $output;
+} else {
+   echo 'Data Not Found';
 }
