37881f1c0bc4c12d7d560e61c875b2209043881b
JannatN <jannatfaisalnatsheh@gmail.com>
2020-12-20 14:05:37 +0200

    edit dept search

--- fetch_dept.php
+++ fetch_dept.php
@@ -10,35 +10,31 @@ if (isset($_POST["submit"])) {
    //   OR last_name LIKE '%" . $search . "%'";
 
    $query = "SELECT first_name, last_name,departments.dept_name 
-FROM employees, departments, dept_emp
-Where dept_emp.emp_no =employees.emp_no 
-AND departments.dept_no =dept_emp.dept_no
-AND departments.dept_name LIKE '$str'";
+            FROM employees, departments, dept_emp
+            Where dept_emp.emp_no =employees.emp_no 
+            AND departments.dept_no =dept_emp.dept_no
+            AND departments.dept_name LIKE '$str'
+            limit 100";
 
-   // } else {
-   //    $query = "SELECT first_name, last_name, departments.dept_name 
-   //    FROM employees, departments, dept_emp
-   //    Where dept_emp.emp_no =employees.emp_no 
-   //    AND departments.dept_no =dept_emp.dept_no";
-   // }
    $result = mysqli_query($connect, $query);
    if (mysqli_num_rows($result) > 0) {
       $output .= '
   <div class="table-responsive">
-   <table class="table table bordered">
-    <tr>
-     <th>First Name</th>
-     <th>last NAme</th>
-    
-    </tr>
- ';
+      <table class="table table bordered">
+      <tr>
+      <th>Department</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      
+      </tr>';
       while ($row = mysqli_fetch_array($result)) {
          $output .= '
-   <tr>
-    <td>' . $row["first_name"] . '</td>
-    <td>' . $row["last_name"] . '</td>
-   </tr>
-  ';
+      <tr>
+      <td>' . $row["dept_name"] . '</td>
+      <td>' . $row["first_name"] . '</td>
+      <td>' . $row["last_name"] . '</td>
+    
+       </tr>';
       }
       echo $output;
    } else {
